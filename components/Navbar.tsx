737c33f07c12c8ccb9c32cf4a52021b829fe764a
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-02 16:19:51 +0200

    create navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="bg-slate-600 text-white p-2">
+      <p className="font-medium text-xl">
+        <span className="text-2xl font-bold text-green-400">C</span>oding
+        <span className="text-2xl font-bold text-green-400">T</span>ravel
+      </p>
+    </div>
+  );
+}
+
+export default Navbar;
