afebfaba2a4f9c7b250a63c83b550a8643a01215
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-02 16:24:01 +0200

    outsourcing style to global

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,8 +4,8 @@ function Navbar() {
   return (
     <div className="bg-slate-600 text-white p-2">
       <p className="font-medium text-xl">
-        <span className="text-2xl font-bold text-green-400">C</span>oding
-        <span className="text-2xl font-bold text-green-400">T</span>ravel
+        <span className="navbar__style">C</span>oding
+        <span className="navbar__style">T</span>ravel
       </p>
     </div>
   );
