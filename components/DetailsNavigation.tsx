dbeceff542740bfe9ceaa3f78050e177490235fc
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-03 18:06:56 +0200

    details navigation separate components

---
+++ components/DetailsNavigation.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React, { useState } from "react";
+import Sights from "@/components/Sights";
+import Reviews from "@/components/Reviews";
+import Description from "@/components/Description";
+import { TravelItem } from "@/type/types";
+
+interface Props {
+  newItem: TravelItem | undefined;
+}
+
+const DetailsNavigation = ({ newItem }: Props) => {
+  const [currentTab, setCurrentTab] = useState("Description");
+  return (
+    <>
+      <nav className="absolute top-0 left-0 right-0 flex justify-between items-center p-4 text-white">
+        <button
+          className={` ${currentTab === "Description" ? "border-b" : ""}`}
+          onClick={() => setCurrentTab("Description")}
+        >
+          Description
+        </button>
+        <button
+          className={` ${currentTab === "Sights" ? "border-b" : ""}`}
+          onClick={() => setCurrentTab("Sights")}
+        >
+          Sights
+        </button>
+        <button
+          className={` ${currentTab === "Reviews" ? "border-b" : ""}`}
+          onClick={() => setCurrentTab("Reviews")}
+        >
+          Reviews
+        </button>
+      </nav>
+      <div className="p-4">
+        {currentTab === "Description" && <Description newItem={newItem} />}
+        {currentTab === "Sights" && <Sights newItem={newItem} />}
+        {currentTab === "Reviews" && <Reviews newItem={newItem} />}
+      </div>
+    </>
+  );
+};
+
+export default DetailsNavigation;
