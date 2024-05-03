d96a6340d913e622a137d960562aab98569d5574
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-03 18:07:30 +0200

    reviews style

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
-const Reviews = () => {
-  return (
-    <div className='p-64'>Reviews</div>
-  )
+import { TravelItem } from "@/type/types";
+import React from "react";
+import Rating from "./Rating";
+interface Props {
+  newItem: TravelItem | undefined;
 }
+const Reviews = ({ newItem }: Props) => {
+  return (
+    <section className=" px-4 py-12 md:py-24 w-full z-50  overflow-scroll">
+      {newItem?.reviews.map((reviews) => (
+        <div key={reviews.id} className="max-w-screen-xl mx-auto m-2 ">
+          <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-4 relative ">
+            <div className="bg-blue-200 rounded-lg p-8 text-center ">
+              <p className="font-bold uppercase">{reviews.name}</p>
+              <p className="text-xl font-light italic text-gray-700">
+                {reviews.description}
+              </p>
+              <div className="flex items-center justify-center space-x-2 mt-4">
+                <Rating rating={reviews.rating} />
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
 
-export default Reviews
+export default Reviews;
