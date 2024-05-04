3f81b38b3e8348ad6bbf8c03eba8f7215af0f899
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-04 18:53:28 +0200

    repair fill and priority

--- components/ImageBg.tsx
+++ components/ImageBg.tsx
@@ -3,14 +3,15 @@ import Image from "next/image";
 function ImageBg() {
   return (
     <div className="relative w-full h-96">
-    <Image
-      src="/images/bg.jpg"       
-      layout="fill"
-      alt="background"
-      className="object-cover	"
-    />
-  </div>
-  )
+      <Image
+        src="/images/bg.jpg"
+        priority
+        fill
+        alt="background"
+        className="object-cover	"
+      />
+    </div>
+  );
 }
 
-export default ImageBg
+export default ImageBg;
