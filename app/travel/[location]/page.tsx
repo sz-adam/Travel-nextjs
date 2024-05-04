1b3e8b01ddb9b0e3527cf91f90d58e4fee62c04f
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-04 18:52:07 +0200

    new style

--- app/travel/[location]/page.tsx
+++ app/travel/[location]/page.tsx
@@ -10,17 +10,20 @@ interface CardProps {
 const TravelDetails = ({ params }: CardProps) => {
   const location = params.location;
   const newItem = Travel.find((item) => item.location === location);
-  console.log(newItem);
 
   return (
-    <div
-      className="bg-cover h-screen w-full flex justify-center items-center"
-      style={{ backgroundImage: `url(${newItem?.image})` }}
-    >
-      <div className="h-5/6 w-5/6 md:h-2/3 md:w-2/3 bg-gray-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100 overflow-auto">
-       <DetailsNavigation newItem={newItem}/>
-      </div>
+    <div className="w-full bg-slate-600 flex max-lg:flex-col ">
+    <div className="h-screen w-full lg:w-8/12 bg-cover">
+      <img
+        src={newItem?.image}
+        alt={newItem?.name}
+        className="bg-cover bg-no-repeat bg-center h-full"
+      />
     </div>
+    <div className="w-full lg:w-4/12 overflow-auto">
+      <DetailsNavigation newItem={newItem} />
+    </div>
+  </div>
   );
 };
 
