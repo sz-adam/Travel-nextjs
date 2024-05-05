304e6a4102cb8d29d663b43743e41f31d0e9de6a
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-05 16:12:47 +0200

    display category

--- components/filter/Filter.tsx
+++ components/filter/Filter.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 import { RiCloseLargeLine } from "react-icons/ri";
+import Category from "./Category";
 
 interface Props {
   showFilter: boolean;
   setShowFilter: (show: boolean) => void;
 }
-const categories = ["Food", "Travel", "Fashion"];
-const continents = [
-  "Europe",
-  "Asia",
-  "Africa",
-  "North America",
-  "South America",
-  "Australia",
-];
+
 const Filter = ({ showFilter, setShowFilter }: Props) => {
   return (
     <div className="z-50 bg-white w-2/3 md:w-1/4 h-screen absolute top-0 flex flex-col">
@@ -23,35 +16,8 @@ const Filter = ({ showFilter, setShowFilter }: Props) => {
           className="cursor-pointer"
         />
       </div>
-      <div className="m-4">
-        <h2 className="text-xl font-bold mb-2">Categories</h2>
-        {categories.map((category, index) => (
-          <div key={index} className="flex items-center">
-            <input
-              type="checkbox"
-              id={category}
-              name={category}
-              className="mr-2"
-            />
-            <label htmlFor={category}>{category}</label>
-          </div>
-        ))}
-      </div>
-      <div className="m-4">
-        <h2 className="text-xl font-bold mb-2">Continents</h2>
-        {continents.map((continent, index) => (
-          <div key={index} className="flex items-center">
-            <input
-              type="checkbox"
-              id={continent}
-              name={continent}
-              className="mr-2"
-            />
-            <label htmlFor={continent}>{continent}</label>
-          </div>
-        ))}
-      </div>
-      <button>Filter</button>
+      <Category />
+    
     </div>
   );
 };
